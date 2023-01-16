89fee97019fd04c9955fa0d671a1e9c22f72bf00
Viktorrrrr <viktorcvetanovic@gmail.com>
2023-01-16 14:36:36 +0100

    Home page works

--- carspot-frontend/src/app/app.module.ts
+++ carspot-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './feature/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
