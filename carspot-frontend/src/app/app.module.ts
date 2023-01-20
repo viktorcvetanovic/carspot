46fa04d39cb6ada87631df8b4af212d5052d767e
Viktorrrrr <viktorcvetanovic@gmail.com>
2023-01-20 13:32:34 +0100

    Created pages

--- carspot-frontend/src/app/app.module.ts
+++ carspot-frontend/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './feature/nav-bar/nav-bar.component';
 import { FooterComponent } from './feature/footer/footer.component';
 import { PostViewComponent } from './post-view/post-view.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { LoginViewComponent } from './login-view/login-view.component';
+import { RegisterViewComponent } from './register-view/register-view.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { PostViewComponent } from './post-view/post-view.component';
     HomePageComponent,
     NavBarComponent,
     FooterComponent,
-    PostViewComponent
+    PostViewComponent,
+    PostCreateComponent,
+    ProfileViewComponent,
+    LoginViewComponent,
+    RegisterViewComponent
   ],
   imports: [
     BrowserModule,
